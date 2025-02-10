16e38eb33f63a186481a1b86426deb1092b761f4
lenkalapellypraveen <praveen.lenkalapelly9@gmail.com>
2025-02-09 21:42:46 -0500

    Updated code

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -41,7 +41,7 @@ const Projects = () => {
                     <div className="mb-4">
                     {/* First Project */}
                     <div className="relative rounded-2xl shadow-lg overflow-hidden flex flex-col group">
-                        <img src={testing_image} alt="" className="w-full transition-all duration-500 ease-in-out transform group-hover:scale-110" />
+                        <img src={Genomic_Ancestry_Prediction} alt="" className="w-full transition-all duration-500 ease-in-out transform group-hover:scale-110" />
                         <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-[rgba(0,0,0,0.1)] to-[#0ef] flex flex-col justify-center items-center text-center p-16 translate-y-full group-hover:translate-y-0 transition-all duration-500 z-10">
                             <h4 className="text-2xl mb-2">Genomic Ancestry Prediction through Advanced Machine Learning Integration</h4>
                             <p className="text-base mb-3">Click the link for more information</p>
