0048cf711f84946f0f9e411b43d696f7404c8e01
lenkalapellypraveen <praveen.lenkalapelly9@gmail.com>
2025-02-10 23:21:59 -0500

    Updated code

--- src/components/Flow.tsx
+++ src/components/Flow.tsx
@@ -15,17 +15,17 @@ const TimelineComponent: React.FC = () => {
       title: "Data Scientist",
       company: "Modak Analytics",
       description: [
-        "• Led the development of large-scale data pipelines using Python, focusing on AI and statistical"
+        "• Led the development of large-scale data pipelines using Python, focusing on AI and statistical",
         " modeling for predictive analytics and complex GenAI challenges.",
-        "• Constructed machine learning models using Azure Machine Learning, boosting model accuracy by 25%."
+        "• Constructed machine learning models using Azure Machine Learning, boosting model accuracy by 25%.",
         " Employed MLOps for streamlined deployment, and feature engineering within the Azure OpenAI.",
-        "• Integrated Azure Databricks with Power BI for real-time data visualization, facilitating "
+        "• Integrated Azure Databricks with Power BI for real-time data visualization, facilitating ",
         "process optimization and data manipulation by reducing report generation time by 35%.",
-        "• Developed SQL frameworks and Python algorithms with Natural Language Processing for trend analysis,"
+        "• Developed SQL frameworks and Python algorithms with Natural Language Processing for trend analysis,",
         " cutting query response time by 30% and boosting management decision efficiency by 15%.",
-        "• Applied complex statistical methods and machine learning algorithms to enhance operational efficiency by 25%,"
+        "• Applied complex statistical methods and machine learning algorithms to enhance operational efficiency by 25%,",
         " seamlessly integrating approaches into various GenAI applications to comply with evolving industry standards.",
-        "• Developed algorithms using Python to analyze real-time data streams via Confluent Kafka, employing research, "
+        "• Developed algorithms using Python to analyze real-time data streams via Confluent Kafka, employing research, ",
         "A/B testing, and applied mathematics to identify emergent patterns, enhancing operational responsiveness by 35%."
       ],
       startDate: "May 2022",
@@ -36,11 +36,11 @@ const TimelineComponent: React.FC = () => {
       title: "Data Engineer",
       company: "Modak Analytics",
       description: [
-        "• Developed bulk ingestion data pipelines using StreamSets, and custom-coded pipelines in Spark-Scala"
+        "• Developed bulk ingestion data pipelines using StreamSets, and custom-coded pipelines in Spark-Scala",
         "/PySpark for ETL operations into Amazon S3 and Hive, enhancing data warehousing accuracy by 20%.",
-        "• Configured AWS Lambda in conjunction with CI/CD tools such as CodePipeline and CodeCommit, automating "
+        "• Configured AWS Lambda in conjunction with CI/CD tools such as CodePipeline and CodeCommit, automating ",
         "data migration processes with Python scripts to eliminate over 50 hours of manual tasks monthly.",
-        "• Optimized business operations by automating data pipeline scheduling with Python DAG scheduler,"
+        "• Optimized business operations by automating data pipeline scheduling with Python DAG scheduler,",
         " orchestrating sequential and dependent data flows to enhance overall data processing efficiency."
       ],
       startDate: "April 2021",
