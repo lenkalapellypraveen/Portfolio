5c98505d21a5b7357f58d4cca2241f213a52cd07
lenkalapellypraveen <praveen.lenkalapelly9@gmail.com>
2025-02-10 20:26:58 -0500

    Updated code

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -23,7 +23,7 @@ const Hero = () => {
       <section
         className={`relative z-30 shrink-0 w-full h-screen mx-auto ${styles.paddingX} pt-[120px] max-w-7xl mx-auto flex items-start justify-between`}
       >
-        <div className="flex flex-col md:flex-row justify-between items-center mt-5 gap-1">
+        <div className="flex flex-col md:flex-row justify-between items-center mt-10 gap-1">
           <div className="flex flex-col gap-1 md:gap-3">
             <p
               className={`${styles.heroSubText} ${
